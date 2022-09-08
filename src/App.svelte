e1920164fbb6f363d0930a4e345b6082bfefa2c5
evariste1963 <evariste@hotmail.co.uk>
2022-09-08 21:46:10 +0100

    up to 1:34:00

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,20 @@
 <script>
   // https://www.youtube.com/watch?v=uk1eM0Yn0UQ
+  //components
   import Navbar from "./Navbar.svelte";
-  import Title from "./Title.svelte";
+  import ExpensesList from "./ExpensesList.svelte";
+  //data
+  import expensesData from "./expenses";
+
+  //variables
+  let expenses = [...expensesData];
 </script>
 
 <Navbar />
-<Title title="add expense" />
-<Title title="expense list" />
-<Title />
+
+<main class="content">
+  <ExpensesList {expenses} />
+</main>
 
 <style>
 </style>
