e1a7c7e1c4f0361e174db63372ca65a10cdf79ef
evariste1963 <evariste@hotmail.co.uk>
2022-09-10 17:08:14 +0100

    1:51:41

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -4,7 +4,22 @@
     id;
 </script>
 
-<div>
-  <h2>title : {name}</h2>
-  <h2>amount : {amount}</h2>
-</div>
+<article class="single-expense">
+  <div class="expense-info">
+    <h2>
+      {name}
+      <button class="amount-btn">
+        <i class="fas fa-caret-down" />
+      </button>
+    </h2>
+    <h4>amount :${amount}</h4>
+  </div>
+  <div class="expense-buttons">
+    <button class="expense-btn edit-btn">
+        <div class="fas fa-pen"></div>
+    </button>
+    <button class="expense-btn delete-btn">
+      <div class="fas fa-trash"></div>
+  </button>
+  </div>
+</article>
