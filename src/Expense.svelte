308e0835f5daacdb5e375287f3a68deeafa881e2
evariste1963 <evariste@hotmail.co.uk>
2022-09-11 20:08:05 +0100

    2:04:40

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -2,24 +2,35 @@
   export let name = "",
     amount = 0,
     id;
+  let displayAmount = false;
+
+  const toggleAmount = () => {
+    displayAmount = !displayAmount;
+  };
 </script>
 
 <article class="single-expense">
   <div class="expense-info">
     <h2>
       {name}
-      <button class="amount-btn">
-        <i class="fas fa-caret-down" />
+      <button class="amount-btn" on:click={toggleAmount}>
+        {#if !displayAmount}
+          <i class="fas fa-caret-down" />
+        {:else}
+          <i class="fas fa-caret-up" />
+        {/if}
       </button>
     </h2>
-    <h4>amount :${amount}</h4>
+    {#if displayAmount}
+      <h4>amount :${amount}</h4>
+    {/if}
   </div>
   <div class="expense-buttons">
     <button class="expense-btn edit-btn">
-        <div class="fas fa-pen"></div>
+      <div class="fas fa-pen" />
     </button>
     <button class="expense-btn delete-btn">
-      <div class="fas fa-trash"></div>
-  </button>
+      <div class="fas fa-trash" />
+    </button>
   </div>
 </article>
